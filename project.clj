3e48edbab5d3e9f9b5d10a80416ae7e77293ec87
Deyan Yotsov <deyan@yotsov.org>
2021-12-16 00:08:11 +0100

    updating dependencies

--- project.clj
+++ project.clj
@@ -5,19 +5,25 @@
   :dependencies [;; backend:
                  [org.clojure/clojure "1.10.3"]
                  [compojure "1.6.2"] ;; library for handing http requests
-                 [ring/ring-defaults "0.3.2"] ;; library for wiring up a web app
-                 [info.sunng/ring-jetty9-adapter "0.14.3"] ;; contains a simple WebSocket library
-                 [org.clojure/data.json "2.3.1"] ;; parsing and encoding json
+                 [ring/ring-defaults "0.3.3"] ;; library for wiring up a web app
+                 [info.sunng/ring-jetty9-adapter "0.16.0"] ;; contains a simple WebSocket library
+                 [org.clojure/data.json "2.4.0"] ;; parsing and encoding json
 
                  ;; frontend:
-                 [org.clojure/clojurescript "1.10.866"] ;; like Clojure but compiles to JavaScript
+                 [org.clojure/clojurescript "1.10.896"] ;; like Clojure but compiles to JavaScript
                  [reagent "1.1.0"] ;; ClojureScript wrapper around React
                  [cljsjs/react "17.0.2-0"]
                  [cljsjs/react-dom "17.0.2-0"]
                  [haslett "0.1.6"]] ;; WebSocket library on the browser side
 
   :plugins [[lein-cljsbuild "1.1.8"] ;; helps compile ClojureScript to JavaScript
-            [lein-cljfmt "0.7.0"]] ;; for formatting Clojure code using the command:
+
+            ;; optional plugins:
+
+            [lein-ancient "1.0.0-RC3"] ;; finds updatable dependencies by running:
+            ;; lein ancient ; echo "plugins:" ; lein ancient :plugins
+
+            [lein-cljfmt "0.8.0"]] ;; for formatting Clojure code using the command:
             ;; lein do cljfmt fix, cljfmt fix src-cljs/squares/frontend.cljs, cljfmt fix project.clj
 
   :aot :all
